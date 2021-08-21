e5d883c481e8a673f60edb2be55f0793e4df0e70
fathur <fathur.jtk09@gmail.com>
2021-08-22 06:03:07 +0700

    check ok

---
+++ basic_syntax.py
@@ -0,0 +1,21 @@
+# first
+
+# skuensial
+print('get up')
+print('okay')
+
+# if
+milk_count = 10
+egg_count = 23
+
+print("Jumlah susu :", milk_count, "botol")
+print("Jumlah telur :", egg_count, "butir")
+print(f"Jumlah telur : {egg_count} butir ")
+
+if milk_count > 0:
+    print("budi buy 1 bottle milk")
+    if egg_count > 0:
+        print("buy 6 eggs")
+else:
+    print("budi not buying anything")
+
