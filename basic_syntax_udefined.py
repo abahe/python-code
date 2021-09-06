f62ca8da1f169c2b8e9b9b4e00181124e48b7422
fathur <fathur.jtk09@gmail.com>
2021-09-07 05:34:54 +0700

    cek

---
+++ basic_syntax_udefined.py
@@ -0,0 +1,50 @@
+# first
+
+# skuensial
+print('get up')
+print('okay')
+
+# if
+milk_count = 10
+egg_count = 23
+
+print("Jumlah susu :", milk_count, "botol")
+print("Jumlah telur :", egg_count, "butir")
+print(f"Jumlah telur : {egg_count} butir ")
+
+if milk_count > 0:
+    print("budi buy 1 bottle milk")
+    if egg_count > 0:
+        print("buy 6 eggs")
+else:
+    print("budi not buying anything")
+
+# repeat for
+sum_book = 5
+print('read all books!')
+
+readed_book = 0
+print(f'Has been read : {readed_book}')
+
+# for readed_book in range(1, sum_book+1):
+#     print(f"Book {readed_book} has been read")
+
+print(f'Has been read : {readed_book}')
+
+print("----")
+print("----")
+
+while readed_book < sum_book:
+    readed_book = readed_book + 1
+    print(f"book {readed_book} has been read")
+
+print(f'Has been read : {readed_book}')
+
+# okay
+
+readed_book = 10
+print(readed_book)
+
+readed_book = "nugelo"
+print(readed_book)
+
